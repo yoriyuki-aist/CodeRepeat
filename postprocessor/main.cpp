b3eb84d87784e9724e09245a122d78848d926ddd
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-06-19 16:33:27 +0200

    Implement bwt output parsing

--- postprocessor/main.cpp
+++ postprocessor/main.cpp
@@ -15,6 +15,55 @@ bool endsWith(std::string const &fullString, std::string const &ending) {
     }
 }
 
+struct RepeatEntry {
+    int size{};
+    int occurrences{};
+    std::string subtext;
+    std::vector<int> positions;
+};
+
+// custom extractor for objects of type RepeatEntry
+std::istream& operator>>(std::istream& is, RepeatEntry& repeat)
+{
+    std::istream::sentry s(is);
+    std::string line;
+    if (s) {
+        std::getline(is, line, ':');
+        if (line != "Repeat size") {
+            throw std::runtime_error("Expected repeat size in first Repeat line");
+        }
+        is >> repeat.size;
+        std::getline(is, line); // discard rest of line
+        std::getline(is, line, ':');
+        if (line != "Number of occurrences") {
+            throw std::runtime_error("Expected number of occurrences in second Repeat line");
+        }
+        is >> repeat.occurrences;
+        std::getline(is, line); // discard rest of line
+        std::getline(is, line, ':');
+        if (line != "Repeat subtext") {
+            throw std::runtime_error("Expected repeat subtext in third Repeat line");
+        }
+        is.get();
+        std::getline(is, repeat.subtext);
+        std::getline(is, line, ':');
+        if (line != "Suffix array interval of this repeat") {
+            throw std::runtime_error("Expected suffix array interval in fourth Repeat line");
+        }
+        std::getline(is, line); // discard suffix array interval
+        std::getline(is, line, ':');
+        if (line != "Text positions of this repeat") {
+            throw std::runtime_error("Expected text positions in fifth Repeat line");
+        }
+        int pos;
+        while (is >> pos) {
+            repeat.positions.push_back(pos);
+        }
+        is.clear();
+    }
+    return is;
+}
+
 int main(int argc, char **argv) {
     if (argc < 3) {
         std::cout << "\nUsage:\t%s\t<bwt_output>\t<charmap_file>\t[<options...>]\n";
@@ -35,8 +84,9 @@ int main(int argc, char **argv) {
     int char_idx;
 
     while (charmap_in >> char_idx) {
-        if (charmap_in.rdbuf()->sgetc() != '\t') {
+        if (charmap_in.rdbuf()->sbumpc() != '\t') {
             std::cerr << "Unexpected character at position " << charmap_in.tellg() << " in " << charmap_file;
+            break;
         }
         std::getline(charmap_in, line);
         charmap[char_idx] = line;
@@ -49,6 +99,16 @@ int main(int argc, char **argv) {
     if (!bwt) {
         std::cout << "bwt output file open fails. exit.\n";
         exit(1);
+    }
+
+    std::vector<RepeatEntry> repeats;
+    RepeatEntry repeat;
+    try {
+        while (bwt >> repeat) {
+            repeats.push_back(repeat);
+        }
+    } catch (std::runtime_error &e) {
+        std::cerr << "Failed to read repeat entry at position " << bwt.tellg() << " in " << bwt_file << ": " << e.what();
     }
 
     bwt.close();
