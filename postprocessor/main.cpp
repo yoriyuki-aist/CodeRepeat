2ce6a62cfe22863529e7dd2e68a4fdfa543e27fe
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-06-25 19:59:33 +0200

    Rewrite postprocessor parsing, fix multifile repeats

--- postprocessor/main.cpp
+++ postprocessor/main.cpp
@@ -4,7 +4,6 @@
 #include <filesystem>
 #include <set>
 #include <regex>
-#include <bits/unordered_set.h>
 #include "json/json.h"
 
 namespace fs = std::filesystem;
@@ -22,60 +21,65 @@ struct RepeatPosition {
     const unsigned int concatpos{};
     // The position of the repeat in the source file (begins at 0!)
     const unsigned int sourcepos{};
-    // the fraction of the total file's length that the repeat represents
-    const double relative_size;
     const std::string source_file;
 };
 
-struct RepeatEntry {
-    int size{};
-    int occurrences{};
-    std::string subtext;
-    std::vector<RepeatPosition> positions;
-};
-
-struct FileRepeats {
-    std::vector<RepeatEntry> repeats;
-    fs::path path;
-};
-
-using extension = std::string;
-using ProcessedRepeats = std::unordered_map<extension, std::vector<FileRepeats>>;
+using path = std::string;
+using Repeats = std::unordered_map<path, std::vector<RepeatPosition>>;
 using CharMap = std::map<unsigned int, std::string>;
 
-Json::Value
-to_json(const ProcessedRepeats &repeats,
-        const CharMap &map);
+void process_position(const CharMap &charmap, Repeats &repeats, std::string subtext, unsigned int pos) {
+    unsigned int repeat_end = pos + subtext.size();
+    auto it = --charmap.upper_bound(pos);
 
-ProcessedRepeats process(const std::vector<RepeatEntry> &vector);
+    do {
+        // beginning of the source file where the start of the repeat is found
+        unsigned int file_begin = it->first;
+        // name of the source file where the start of the repeat is found
+        const std::string &source_file = it->second;
+        it++;
+        // beginning of the next file after the start of the repeat
+        unsigned int file_end = it->first;
+        std::string repeat_subtext;
 
-RepeatPosition find_repeat_position(unsigned int concatpos, const CharMap &charmap, int repeat_length) {
-    auto next_entry = charmap.upper_bound(concatpos);
-    unsigned int file_end = next_entry->first;
-    const std::pair<unsigned int, std::string> &entry = *(--next_entry);
-    unsigned int file_begin = entry.first;
-    unsigned int file_size = file_end - file_begin;
-    double relative_length = (double) repeat_length / file_size;
-    return {concatpos, concatpos - entry.first, relative_length, entry.second};
+        if (it == charmap.end() || repeat_end <= file_end) {
+            // there is no next file, or the repeat fits in the current file -> no more processing required
+            repeat_subtext = std::move(subtext);
+            subtext.clear();
+        } else {
+            // the repeated sequence spans multiple files -> split it
+            unsigned int actual_size = file_end - pos;
+            repeat_subtext = subtext.substr(0, actual_size);
+            subtext = subtext.substr(actual_size);
+        }
+
+        repeats[repeat_subtext].push_back({pos, pos - file_begin, source_file});
+    } while (!subtext.empty());
 }
 
 // custom extractor for objects of type RepeatEntry
-std::istream &read(std::istream &is, RepeatEntry &repeat, const CharMap &charmap) {
-    repeat.positions.clear();
+void read(std::istream &is, Repeats &repeats, const CharMap &charmap) {
     std::istream::sentry s(is);
     std::string line;
+
     if (s) {
         std::getline(is, line, ':');
+
         if (line != "Repeat size") {
             throw std::runtime_error("Expected repeat size in first Repeat line");
         }
-        is >> repeat.size;
+
+        unsigned int repeat_size;
+        is >> repeat_size;
         std::getline(is, line); // discard rest of line
         std::getline(is, line, ':');
+
         if (line != "Number of occurrences") {
             throw std::runtime_error("Expected number of occurrences in second Repeat line");
         }
-        is >> repeat.occurrences;
+
+        unsigned int repeat_occurrences;
+        is >> repeat_occurrences;
         std::getline(is, line); // discard rest of line
         std::getline(is, line, ':');
 
@@ -84,29 +88,29 @@ std::istream &read(std::istream &is, RepeatEntry &repeat, const CharMap &charmap
         }
 
         is.get();   // discard the space immediately after
-        char subtext[repeat.size];
-        is.read(subtext, repeat.size);
-        repeat.subtext = std::string(subtext, subtext + repeat.size);
+        char subtext[repeat_size];
+        is.read(subtext, repeat_size);
+        std::string repeat_subtext(subtext, subtext + repeat_size);
         std::getline(is, line);    // get rid of the end of the line
         std::getline(is, line, ':');
 
-        if (line !="Suffix array interval of this repeat") {
+        if (line != "Suffix array interval of this repeat") {
             throw std::runtime_error("Expected suffix array interval in fourth Repeat line");
         }
 
         std::getline(is, line);    // discard suffix array interval
-
         std::getline(is, line, ':');
+
         if (line != "Text positions of this repeat") {
             throw std::runtime_error("Expected text positions in fifth Repeat line");
         }
-        unsigned int pos;
-        while (is >> pos) {
-            repeat.positions.push_back(find_repeat_position(pos, charmap, repeat.size));
+
+        for (int i = 0; i < repeat_occurrences; i++) {
+            unsigned int pos;
+            is >> pos;
+            process_position(charmap, repeats, repeat_subtext, pos);
         }
-        is.clear();
     }
-    return is;
 }
 
 int main(int argc, char **argv) {
@@ -147,11 +151,10 @@ int main(int argc, char **argv) {
         exit(1);
     }
 
-    std::vector<RepeatEntry> repeats;
-    RepeatEntry repeat;
+    Repeats repeats;
     try {
-        while (read(bwt, repeat, charmap)) {
-            repeats.push_back(repeat);
+        while (bwt) {
+            read(bwt, repeats, charmap);
         }
     } catch (std::runtime_error &e) {
         std::cerr << "Failed to read repeat entry at position " << bwt.tellg() << " in " << bwt_file << ": "
@@ -159,76 +162,5 @@ int main(int argc, char **argv) {
     }
 
     bwt.close();
-
-    ProcessedRepeats processedRepeats = process(repeats);
-
-    std::ofstream json_out(json_file);
-
-    if (!json_out) {
-        std::cout << "json output file open fails. exit.\n";
-        exit(1);
-    }
-
-    json_out << to_json(processedRepeats, charmap);
-
-    json_out.close();
-}
-
-ProcessedRepeats process(const std::vector<RepeatEntry> &vector) {
-    std::unordered_map<std::string, std::unordered_set<const RepeatEntry*>> file2repeats;
-
-    for (const RepeatEntry &repeat : vector) {
-        for (const RepeatPosition &position : repeat.positions) {
-            file2repeats[position.source_file].insert(&repeat);
-        }
-    }
-
-    std::unordered_map<extension, std::vector<FileRepeats>> result;
-
-    for (const auto &f2r : file2repeats) {
-        fs::path file(f2r.first);
-        std::vector<RepeatEntry> repeats;
-
-        for (const auto &repeat : f2r.second) {
-            repeats.push_back(*repeat);
-        }
-
-        result[file.extension()].push_back({repeats, file});
-    }
-
-    return result;
-}
-
-Json::Value to_json(const ProcessedRepeats &repeats, const CharMap &charmap) {
-    Json::Value root;
-    root["version"] = 0;
-
-    for (const auto &extension : repeats) {
-        for (const auto &file_repeats : extension.second) {
-            Json::Value filejson;
-            filejson["path"] = std::string(file_repeats.path);
-
-            for (const auto &repeat : file_repeats.repeats) {
-                Json::Value repeatjson;
-                repeatjson["size"] = repeat.size;
-                repeatjson["occurrences"] = repeat.occurrences;
-                repeatjson["subtext"] = repeat.subtext;
-
-                for (const RepeatPosition &pos : repeat.positions) {
-                    Json::Value posjson;
-                    posjson["concat_position"] = pos.concatpos;
-                    posjson["source_position"] = pos.sourcepos;
-                    posjson["source_file"] = pos.source_file;
-                    posjson["relative_size"] = pos.relative_size;
-                    repeatjson["positions"].append(posjson);
-                }
-
-                filejson["repeats"].append(repeatjson);
-            }
-
-            root[extension.first].append(filejson);
-        }
-    }
-
-    return root;
+    std::cout << "Finished parsing BWT output";
 }
