c6a611d8ee5b7ea1f3f9a61a0bacd1a68ac58ad0
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-06-29 13:50:52 +0200

    Convert env to args in test.sh

--- test.sh
+++ test.sh
@@ -1,39 +1,60 @@
 if [ -z $1 ]; then
-  echo "Usage: $0 <src> [<destfolder>]"
-  echo "Run '$0 -h' for more detailed instructions"
+  echo "Usage: $0 <pre|findrepeats|post|all|help>"
   exit 1
 fi
 
-if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+shift;
+
+if [[ "$1" == "help" || "$1" == "-h" ]]; then
   echo "
 Execution script for the clone detection tool
-Usage: $0 <src> [<destfolder>]
+Usage: $0 <pre|findrepeats|post|all|help>
 
 If no destination folder is specified, output files get written to the parent of the source folder.
 
-Environment variables:
-  - \$PREFIX: location of the cmake build directory for the project. Must be specified.
-  - \$EXTENSIONS: list of file extension_filter to process, separated by spaces. Defaults to processing every file.
-  - \$INTERMEDIARIES: output directory for intermediary files. Defaults to <destfolder>.
-  - \$MIN_REPEAT_LENGTH: minimum length of processed repeated sequences. Defaults to 10.
+Arguments:
+  -i <dir>: input directory for the script. Must be specified.
+  --prefix <dir>: location of the cmake build directory for the project. Must be specified.
+  --extensions <exts...>: quoted list of file extensions to process, separated by spaces. Defaults to processing every file.
+  --intermediaries <dir>: output directory for intermediary files. Defaults to <destfolder>.
+  --min-repeat-length <nb>: minimum length of processed repeated sequences. Defaults to 10.
 "
   exit 0
 fi
 
+while true; do
+  case "$1" in
+    -i )
+      src="${2%/}"; shift 2 ;;
+    --prefix )
+      PREFIX="$2"; shift 2 ;;
+    --extensions )
+      EXTENSIONS="$2"; shift 2 ;;
+    --intermediaries ) INTERMEDIARIES="$2"; shift 2 ;;
+    --min-repeat-length ) MIN_REPEAT_LENGTH="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+  esac
+done
+
+if [ -z "$src" ]; then
+  echo "The -i argument must be set to the location of the input directory."
+  exit 1
+fi
+
 if [ -z "$PREFIX" ]; then
-  echo "The PREFIX environment variable must be set to the location of the cmake build directory."
+  echo "The --prefix argument must be set to the location of the cmake build directory."
   exit 1
 fi
 
 if [ -z "$EXTENSIONS" ]; then
-  read -rp "Empty EXTENSIONS environment variable, every file in $src will be processed. Press any key to proceed.
+  read -rp "Empty --extensions argument, every file in $src will be processed. Press any key to proceed.
 "
   extension_filter=""
 else
   extension_filter="--extensions $EXTENSIONS"
 fi
 
-src="${1%/}" # remove trailing slash
 if [ -z "$2" ]; then
   dest=$(dirname "$src")
 else
