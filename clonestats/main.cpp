8428f4e893f4028e7a61c7d5d6f95551a6c401ef
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-07-07 01:16:07 +0200

    Add CSV output for idiom detection

--- clonestats/main.cpp
+++ clonestats/main.cpp
@@ -12,8 +12,13 @@ void parse_json(std::map<unsigned long, std::string> &extensions,
                 Statistics &stats,
                 std::ifstream &json_in);
 
-void print_results(const std::unordered_map<std::string, std::map<unsigned long, OccurrenceCounter>> &occurrences,
-                   std::ostream &out);
+void print_occurrence_counts(const std::unordered_map<std::string, std::map<unsigned long, OccurrenceCounter>> &occurrences,
+                             std::ostream &out);
+
+void
+print_idioms(std::vector<RepeatDigest> repeats, const std::string &idiom_rate, std::ostream &out);
+
+void print_results(const Statistics &stats, const std::optional<std::string> &idiom_rate, std::ostream &out);
 
 namespace fs = std::filesystem;
 
@@ -39,36 +44,57 @@ int main(int argc, char **argv) {
     parse_json(extensions, stats, json_in);
     json_in.close();
 
-    // Number of occurrences of repeated subsequences by file extension
-    const std::unordered_map<std::string, std::map<unsigned long, OccurrenceCounter>> &occurrences = stats.occurrences;
+    std::optional<std::string> idiom_rate = args.getCmdArg("--idioms");
 
     if (out_file) {
         std::ofstream out(*out_file);
-        print_results(occurrences, out);
+        print_results(stats, idiom_rate, out);
     } else {
-        print_results(occurrences, std::cout);
-    }
-
-    std::optional<std::string> idiom_rate = args.getCmdArg("--idioms");
-
-    if (idiom_rate) {
-        std::cout << "Idioms:\n";
-
-        std::sort(stats.repeats.begin(), stats.repeats.end(), [](RepeatDigest &e1, RepeatDigest &e2) {
-            return e1.occurrences < e2.occurrences;
-        });
-
-        auto idiom_start = (unsigned) (stats.repeats.size() * (1.0 - std::stod(*idiom_rate)));
-
-        for (unsigned i = idiom_start; i < stats.repeats.size(); i++) {
-            RepeatDigest &repeat = stats.repeats[i];
-            std::cout << "- '" << repeat.text << "': " << repeat.occurrences << "\n";
-        }
+        print_results(stats, idiom_rate, std::cout);
     }
 }
 
-void print_results(const std::unordered_map<std::string, std::map<unsigned long, OccurrenceCounter>> &occurrences,
-                   std::ostream &out) {
+void print_results(const Statistics &stats, const std::optional<std::string> &idiom_rate, std::ostream &out) {
+    if (idiom_rate) {
+        // Print list of repeated sequences with their frequency
+        print_idioms(stats.repeats, *idiom_rate, out);
+    } else {
+        // Print number of occurrences of repeated subsequences by file extension
+        print_occurrence_counts(stats.occurrences, out);
+    }
+}
+
+void
+print_idioms(std::vector<RepeatDigest> repeats, const std::string &idiom_rate, std::ostream &out) {
+    out << "Idiom,Occurrences\n";
+
+    std::sort(repeats.begin(), repeats.end(), [](auto &e1, auto &e2) {
+        return e1.occurrences < e2.occurrences;
+    });
+
+    auto idiom_start = (unsigned) (repeats.size() * (1.0 - std::stod(idiom_rate)));
+
+    for (unsigned i = idiom_start; i < repeats.size(); i++) {
+        const RepeatDigest &repeat = repeats[i];
+        std::string subtext = repeat.text;
+        size_t index = 0;
+
+        while (true) {
+            index = subtext.find('"', index);
+            if (index == std::string::npos) break;
+
+            /* Make the replacement. */
+            subtext.replace(index, 1, "\"\"");
+
+            /* Advance index forward so the next iteration doesn't pick it up as well. */
+            index += 2;
+        }
+        out << '"' << subtext << "\"," << repeat.occurrences << "\n";
+    }
+}
+
+void print_occurrence_counts(const std::unordered_map<std::string, std::map<unsigned long, OccurrenceCounter>> &occurrences,
+                             std::ostream &out) {
     out << "File extension, Size, Occurrence(s), unique sequence(s)\n";
     for (const auto &ext_entry : occurrences) {
         for (const auto &size_entry : ext_entry.second) {
