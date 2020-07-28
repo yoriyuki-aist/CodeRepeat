06276e1f0c3ba320f3d592adb517475e99fcabe1
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-07-28 23:40:01 +0200

    Fix test bash script

--- test_clone_detection.sh
+++ test_clone_detection.sh
@@ -1,8 +1,6 @@
 loc=$(realpath $1)
 origin=$(pwd)
 cd "$(dirname $0)" || exit
-pwd
-ls "$loc"
-./detect_clones.py '--prefix=cmake_build_debug' scan "$loc" -o 'test.json' > /dev/null &&\
-./detect_clones.py '--prefix=cmake_build_debug' stats 'test.json'
+./detect_clones.py '--prefix=cmake-build-debug' scan "$loc" -o '/tmp/test.json' >& /dev/null &&\
+./detect_clones.py '--prefix=cmake-build-debug' stats '/tmp/test.json' >& /dev/null
 cd "$origin" || exit
