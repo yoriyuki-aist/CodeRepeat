a7438396f1133da0c98a2c79653a66686e912e95
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-08-03 17:24:25 +0200

    Improve bash test script

--- test_clone_detection.sh
+++ test_clone_detection.sh
@@ -1,7 +1,6 @@
-loc=$(realpath $1)
-origin=$(pwd)
-cd "$(dirname $0)" || exit
-./detect_clones.py '--prefix=cmake-build-debug' scan "$loc" -o '/tmp/test.json' >& /dev/null &&\
-./detect_clones.py '--prefix=cmake-build-debug' stats '/tmp/test.json' -o '/tmp/out.csv' >& /dev/null
-cat /tmp/out.csv
-cd "$origin" || exit
+src=$(dirname "$(realpath "$0")")
+tmpdir=/tmp/test_clone_detection/
+mkdir -p "$tmpdir"
+"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" scan "$1" --linemap "$tmpdir/linemap" -o "$tmpdir/test.json" >& /dev/null &&\
+"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" stats "$tmpdir/test.json" -o "$tmpdir/out.csv" >& /dev/null &&\
+cat "$tmpdir/out.csv"
