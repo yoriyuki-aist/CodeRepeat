c8951c955b8ce5a05c64ec82cc9b5593b4dee7f3
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-08-06 19:39:29 +0200

    Update test bash script

--- test_clone_detection.sh
+++ test_clone_detection.sh
@@ -1,6 +1,6 @@
 src=$(dirname "$(realpath "$0")")
 tmpdir=/tmp/test_clone_detection/
 mkdir -p "$tmpdir"
-"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" scan "$1" --linemap "$tmpdir/linemap" -o "$tmpdir/test.json" >& /dev/null &&\
-"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" stats "$tmpdir/test.json" --bigcloneeval -o "$tmpdir/out.csv" >& /dev/null &&\
+"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" scan "$1" --linemap -o "$tmpdir/test.json" >& /dev/null || exit 1
+"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" stats "$tmpdir/test.json" --bigcloneeval -o "$tmpdir/out.csv" >& /dev/null || exit 1
 cat "$tmpdir/out.csv"
