46dadd4dc11a20e4d1128104458426516a9b4ce0
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-08-12 11:09:53 +0200

    Reduce bloat in test script

--- test_clone_detection.sh
+++ test_clone_detection.sh
@@ -1,6 +1,7 @@
 src=$(dirname "$(realpath "$0")")
 tmpdir=/tmp/test_clone_detection
 mkdir -p "$tmpdir"
-"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" scan "$1" --linemap -o "$tmpdir/test.json" >& /dev/null || exit 1
+rm "$tmpdir/*"
+"$src"/detect_clones.py "--prefix=$src/cmake-build-debug" scan "$1" -m 40 --linemap -o "$tmpdir/test.json" >& /dev/null || exit 1
 "$src"/detect_clones.py "--prefix=$src/cmake-build-debug" stats "$tmpdir/test.json" --bigcloneeval -o "$tmpdir/out.csv" >& /dev/null || exit 1
 cat "$tmpdir/out.csv"
