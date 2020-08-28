bdab1d68f53c979ff22432241766c8bacc9fe63b
Fabien Hervé <fabien.herve1@etu.univ-nantes.fr>
2020-08-28 18:08:41 +0200

    Remove now unused modules

--- bwt-juha/dependencies.mk
+++
@@ -1,9 +0,0 @@
-bwt.o: bwt.cpp bwt.hpp dcsuffixsort.hpp stringsort.hpp partition.hpp \
- difference_cover.hpp dcsamplesort.hpp doubling.hpp distribute.hpp \
- kmpsplit.hpp
-ibwt.o: ibwt.cpp ibwt.hpp
-dnabwt.o: dnabwt.cpp dnavector.hpp bwt.hpp dcsuffixsort.hpp \
- stringsort.hpp partition.hpp difference_cover.hpp dcsamplesort.hpp \
- doubling.hpp distribute.hpp kmpsplit.hpp
-bwtsa.o: bwtsa.cpp suffixsort.hpp doubling.hpp partition.hpp \
- stringsort.hpp checker.hpp difference_cover.hpp timing.hpp
