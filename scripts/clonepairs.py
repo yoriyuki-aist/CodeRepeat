b5be7bfb10c9cfa0165bdf31b9395eb706d6342a
Yoriyuki Yamagata <yoriyuki.yamagata@aist.go.jp>
2020-10-15 14:19:53 +0900

    bug fix

--- scripts/clonepairs.py
+++ scripts/clonepairs.py
@@ -7,8 +7,8 @@ import sys
 from collections import namedtuple
 from tqdm import tqdm
 
-input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='backslashreplace')
-output_stream = io.TextIOWrapper(sys.stdout.buffer, encoding='backslashreplace')
+input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='ascii', error='backslashreplace')
+output_stream = io.TextIOWrapper(sys.stdout.buffer, encoding='ascii', error=''backslashreplace')
 
 
 def output_repeat(repeat):
--- scripts/filter.py
+++ scripts/filter.py
@@ -4,8 +4,8 @@ import json
 import io
 import sys
 
-input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='backslashreplace')
-output_stream = io.TextIOWrapper(sys.stdout.buffer, encoding='backslashreplace')
+input_stream = io.TextIOWrapper(sys.stdin.buffer, encoding='ascii', error='backslashreplace')
+output_stream = io.TextIOWrapper(sys.stdout.buffer, encoding='ascii', error='backslashreplace')
 
 parser = argparse.ArgumentParser(description='Filter repeats')
 parser.add_argument('-m', '--min', metavar='N', type=int, default=0,
